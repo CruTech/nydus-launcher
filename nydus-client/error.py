7cf4e8c58a30777a38601f2ae69485de4589bdd7
Christopher Irving <the.earthlord3@gmail.com>
2024-09-14 21:29:01 +1000

    Finding Minecraft dir

---
+++ nydus-client/error.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# If any unrecoverable errors occur, this module reports
+# the error message to the user with a graphical window.
+
+# TODO: make this actually create a window with a message
+def report_fatal_errors(msg):
+    print(msg)
