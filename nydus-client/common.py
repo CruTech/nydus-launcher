99132d82f87c5fef1c6c533c2d839d046be07364
Christopher Irving <the.earthlord3@gmail.com>
2024-10-27 15:09:14 +1100

    strict lstrip and rstrip

--- nydus-client/common.py
+++ nydus-client/common.py
@@ -78,6 +78,36 @@ def get_minecraft_path():
 
     if not os.is_dir(minecraft_path):
         raise OSError("User's Minecraft directory does not exist at {}".format(minecraft_path))
-    
+
+# Python's default lstrip, given a to_strip string of more than one character,
+# will strip everything off the original string until it finds a character
+# which does not appear in the to_strip string at all.
+# i.e. 'https://thog.com'.lstrip('https://') will return 'og.com', not 'thog.com'
+# becuase the 't' and 'h' in 'thog' are also characters in 'https://'.
+# This function strips only the exact to_strip string provided, and doesn't
+# change the original string if it doesn't start with to_strip.
+# So strict_lstrip('https://thog.com', 'https://') will return 'thog.com'
+# and strict_lstrip('https:google', 'ht') will return 'tps:google'
+def strict_lstrip(orig, to_strip):
+    assert isinstance(orig, str), "non-string provided as orig to strict_lstrip: {}".format(orig)
+    assert isinstance(to_strip, str), "non-string provided as to_strip to strict_lstrip: {}".format(to_strip)
+
+    if not orig.startswith(to_strip):
+        return orig
+
+    new = orig[len(to_strip):]
+    return new
+
+# strict_rstrip is to rstrip as strict_lstrip is to lstrip
+def strict_rstrip(orig, to_strip):
+    assert isinstance(orig, str), "non-string provided as orig to strict_rstrip: {}".format(orig)
+    assert isinstance(to_strip, str), "non-string provided as to_strip to strict_rstrip: {}".format(to_strip)
+
+    if not orig.endswith(to_strip):
+        return orig
+
+    new = orig[:len(orig) - len(to_strip)]
+    return new
+
 if __name__ == "__main__":
     print(get_minecraft_path())
