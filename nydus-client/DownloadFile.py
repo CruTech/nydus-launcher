fac1a27eb1b96c5a5ee67f0f21e7466ab92da861
Christopher Irving <the.earthlord3@gmail.com>
2024-11-14 19:29:58 +1100

    Create path

--- nydus-client/DownloadFile.py
+++ nydus-client/DownloadFile.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 import common
+import os
+
+MC_MODE=0o775
 
 class DownloadFile:
 
     # url must be correct according to common.is_download_url
     # path, if given, must be absolute
     # path need not exist
+    # path INCLUDES the final filename
     def __init__(self, name, url, sha1, path=""):
         self.name = name
 
@@ -35,3 +39,22 @@ class DownloadFile:
             upath = upath.lstrip("/")
 
         self.path = "{}/{}".format(mcpath, upath)
+
+    # Create the needed directories to store the file in
+    # But only if they don't exist
+    def create_path(self):
+        # self.path includes final filename; we need
+        # the path without the file to create intermediate directories
+        
+        # TODO
+        # does makedirs have some return code or exception raise
+        # that should be handled here?
+        # it can raise FileExistsError, but we check that
+        # before running it anyway
+        dirpath = os.path.dirname(self.path)
+        if not os.path.isdir(dirpath):
+            # Make sure to create the new directories with the right
+            # permissions, but set the umask back when we're done
+            old_umask = os.umask(MC_MODE)
+            os.makedirs(dirpath, MC_MODE)
+            os.umask(old_umask)
